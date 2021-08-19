880bcee4b6ae60b65ca0b49854667aa2081c009a
rosindubh <p.welsby2@ntlworld.com>
2021-08-19 01:44:27 +0100

    change value of bored in main animal class

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 //phil welsby 18 aug 2021
+
+// variables
 let playerName = prompt("Enter your name here:") //input is the players name
 hungerThreshold = 30;
 thirstThreshold = 60;
 boredThreshold = 70;
+
 //animal class
 class animal {
     constructor(name) {
         this._name = name;
         this._hunger = 100;
         this._thirst = 100;
-        this._bored = 100;
+        this._bored = 59;
     }
     get name() {
         return this._name;
@@ -30,12 +33,15 @@ console.log(`Hi I'm the cat and my name is ${theCat.name}`);
 console.log(`My hunger is a ${theCat.hunger}`);
 console.log(`My thirst is a ${theCat.thirst}`);
 console.log(`My bordem is a ${theCat.bored}`)
+
 if (theCat.hunger < 30) {
     console.log(`${theCat.name} the cat is hungry, my hunger needs to be ${hungerThreshold} and it is currently ${theCat._hunger}`);
 };
 if (theCat.thirst < 60) {
     console.log(`${theCat.name} the cat is thirsty. My thirst needs to be ${thirstThreshold} and it's currently ${theCat.thirst} `)
 };
-if (theCat.bored < 60) {
+if (theCat.bored < 70) {
     console.log(`${theCat.name} the cat is bored play with me. My bordem is ${theCat.bored} and it needs to be ${boredThreshold}`)
-}
+};
+
+
